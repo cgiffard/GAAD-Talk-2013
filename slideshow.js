f613f08f0f6144f487ce4bbccb6ec9bc00b5f823
Christopher Giffard <christopher.giffard@cgiffard.com>
2013-05-09 11:50:58 +1000

    Updated theme and framework

--- slideshow.js
+++ slideshow.js
@@ -1,49 +1,37 @@
 ;(function() {
-	var cSlide = 0,
+	"use strict";
+	
+	var s, cSlide = 0, on = "addEventListener",
 		slides = [].slice.call(document.querySelectorAll("slide"),0),
-		slideAdvanceKeys = [13,32,38,39],
-		slideReverseKeys = [37,40];
+		nextKeys = [13,32,38,39], prevKeys = [37,40],
+		html = document.documentElement;
 	
-	function nextSlide() {
-		goToSlide(cSlide < slides.length -1 ? cSlide + 1 : slides.length -1);
+	function next() {
+		if (cSlide < slides.length-1) slide(cSlide + 1);
 	}
 	
-	function previousSlide() {
-		goToSlide(cSlide > 0 ? cSlide - 1 : 0);
+	function previous() {
+		if (cSlide > 0) slide(cSlide - 1);
 	}
 	
-	function goToSlide(num) {
-		location.hash = cSlide = num;
-		document.documentElement.className = "slide" + cSlide;
+	window.slide = function slide(n) {
+		html.className = "slide" + (location.hash = cSlide = n);
 		
 		slides.forEach(function(slide) {
-			var sNum = +slide.getAttribute("number");
-			
-			if (sNum < num)
-				slide.className = "past";
-			
-			if (sNum === num)
-				slide.className = "present";
-			
-			if (sNum > num)
-				slide.className = "future";
+			s = +slide.getAttribute("number")|0;
+			slide.className = ["past","present","future"][s<n?0:s===n?1:2];
 		});
 	}
 	
-	window.addEventListener("keyup",function(evt) {
-		if (slideAdvanceKeys.indexOf(evt.keyCode) > -1)
-			nextSlide();
-		
-		if (slideReverseKeys.indexOf(evt.keyCode) > -1)
-			previousSlide();
-	});
+	window[on]("keyup",function(evt) {
+		if (nextKeys.indexOf(evt.keyCode) > -1) next();
+		if (prevKeys.indexOf(evt.keyCode) > -1) previous();
+	},!1);
+	
+	window[on]("load",function() {
+		slide(+location.hash.substr(1)|0 || 0);
+	},!1);
 	
 	// Fullscreen body on demand
-	document.documentElement.addEventListener("click",function() {
-		document.documentElement.webkitRequestFullScreen();
-	});
-	
-	window.addEventListener("load",function() {
-		goToSlide(+location.hash > 0 ? +location.hash : 0);
-	});
-})();
+	html[on]("click",(html.webkitRequestFullScreen||html.mozRequestFullScreen),!1);
+}());
